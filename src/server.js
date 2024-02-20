8af185493bf3257b6708292b5a3d9f290a77bc62
Mathew May <29102292+mghmay@users.noreply.github.com>
2024-02-20 20:46:10 +0000

    Update cart functionality

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import {MongoClient} from "mongodb";
 import path from "path";
+
 const app = express();
 const port = 8000;
 
@@ -29,12 +30,29 @@ app.post("/api/users/:userId/cart", async (req, res) => {
 	const product = await db.collection("products").findOne({id: productId});
 	if (!product) return res.status(404).json("Could not find the product");
 
-	await db.collection("users").updateOne(
-		{id: userId},
-		{
-			$push: {cartItems: product},
-		}
-	);
+	const userCart = user.cartItems;
+
+	const item =
+		userCart.length > 0
+			? userCart.find((item) => item.product.id === productId)
+			: undefined;
+
+	if (item) {
+		const amount = item.amount;
+		await db
+			.collection("users")
+			.updateOne(
+				{id: userId, "cartItems.product.id": productId},
+				{$set: {"cartItems.$.amount": amount + 1}}
+			);
+	} else {
+		await db.collection("users").updateOne(
+			{id: userId},
+			{
+				$push: {cartItems: {product, amount: 1}},
+			}
+		);
+	}
 	const updatedUser = await db.collection("users").findOne({id: userId});
 
 	res.status(200).json(updatedUser.cartItems);
@@ -47,23 +65,31 @@ app.delete("/api/users/:userId/cart/:productId", async (req, res) => {
 
 	const user = await db.collection("users").findOne({id: userId});
 	if (!user) return res.status(404).json("could not find user");
-	const cartItems = user.cartItems;
 
-	const indexToDelete = cartItems.findIndex(
-		(product) => product.id === productId
-	);
+	const product = await db.collection("products").findOne({id: productId});
+	if (!product) return res.status(404).json("Could not find the product");
 
-	const updatedCart = [
-		...cartItems.slice(0, indexToDelete),
-		...cartItems.slice(indexToDelete + 1),
-	];
+	const userCart = user.cartItems;
 
-	await db.collection("users").updateOne(
-		{id: userId},
-		{
-			$set: {cartItems: updatedCart},
-		}
-	);
+	const item = userCart.find((item) => item.product.id === productId);
+	if (!item) return res.status(404).json("Could not find the product");
+
+	if (item.amount > 1) {
+		const amount = item.amount;
+		await db
+			.collection("users")
+			.updateOne(
+				{id: userId, "cartItems.product.id": productId},
+				{$set: {"cartItems.$.amount": amount - 1}}
+			);
+	} else {
+		await db.collection("users").updateOne(
+			{id: userId},
+			{
+				$pull: {cartItems: {"product.id": productId}},
+			}
+		);
+	}
 
 	const updatedUser = await db.collection("users").findOne({id: userId});
 	const updatedCartItems = updatedUser.cartItems;
@@ -76,7 +102,9 @@ app.get("/api/users/:userId/cart", async (req, res) => {
 	const client = await MongoClient.connect("mongodb://192.168.0.21:27017");
 	const db = client.db("vue-db");
 
-	const user = await db.collection("users").findOne({id: req.params.userId});
+	const userId = req.params.userId;
+
+	const user = await db.collection("users").findOne({id: userId});
 	if (!user) return res.status(404).json("could not find user");
 
 	res.status(200).json(user.cartItems);
