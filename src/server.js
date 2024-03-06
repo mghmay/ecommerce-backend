777617c36e80172ce12edc7e8eaaf70bababf9da
Mathew May <29102292+mghmay@users.noreply.github.com>
2024-03-06 14:37:28 +0000

    Allow cors all origins

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import {router} from "./routes/router";
+import {cors} from "cors";
 import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/images", express.static(path.join(__dirname, "../assets")));
 app.use("/api", router);
